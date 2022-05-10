d6f4806e643d29674fbda73dee659d8ba98746d4
Akshaynie Mishra <84663060+Aksh55@users.noreply.github.com>
2022-05-10 18:50:30 +0530

    Add files via upload

---
+++ living_room.js
@@ -0,0 +1,26 @@
+function preload()
+{
+    img = loadImage('living.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+}
+
+function draw()
+{
+    image(img , 0 , 0 , 640 , 420);
+}
+
+function back()
+{
+    window.location = "index.html" ;
+}
